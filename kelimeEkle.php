46c994d437d5577c783b06cc16aac5435b18f853
omerfbas <omerbasdev@gmail.com>
2020-02-29 23:40:13 +0300

    Adding synonyms to the database

---
+++ kelimeEkle.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+
+<?php 
+include "db_connect.php";
+include 'Classes/PHPExcel/IOFactory.php';
+
+//  Read your Excel workbook
+$tmpfname = "esanlamli.xlsx";
+		$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+		$excelObj = $excelReader->load($tmpfname);
+		$worksheet = $excelObj->getSheet(0);
+		$lastRow = $worksheet->getHighestRow();
+		
+		echo "<table>";
+		for ($row = 2; $row <= $lastRow; $row++) {
+			$db->exec("INSERT INTO kelimeler(kelime1,kelime2) VALUES('".$worksheet->getCell('A'.$row)->getValue()."','".$worksheet->getCell('B'.$row)->getValue()."')");
+		}
+
+?>
+	
+	
+</body>
+</html>
