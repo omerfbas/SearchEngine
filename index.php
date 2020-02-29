8b769c61bdd6bca69f1e5eb36c7b8a5ce87ea268
omerfbas <omerbasdev@gmail.com>
2020-02-29 23:34:52 +0300

    Index page

---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<?php 
+/*require_once "panel/piston/config/db_connect.php"; */
+ 
+extract($_GET);
+require_once "inc/ust.php"; 
+require_once "inc/function.php"; 
+
+if(isset($_GET["page"]) && file_exists("pages/".$_GET["page"].".php")){
+	$get_page = $_GET["page"];
+	require_once "pages/".$_GET["page"].".php";
+}else{
+	$get_page = "index";
+	require_once "pages/index.php";
+}
+
+?>
+
+<?php require_once "inc/alt.php"; ?>
